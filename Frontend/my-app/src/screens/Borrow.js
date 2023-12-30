8241ec6705476ee4744b87e38ef38e67b97f2db3
tanda <21520714@gm.uit.edu.vn>
2023-12-30 20:04:40 +0700

    Add book details and input field for quantity in Borrow.js, update navigation bar in HomeScreen.js

--- Frontend/my-app/src/screens/Borrow.js
+++ Frontend/my-app/src/screens/Borrow.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import HomeScreen from './HomeScreen'
-
+import {useState, useEffect} from 'react'
 function Borrow() {
     const bookId = localStorage.getItem('bookId');
-    console.log(bookId);
+    const [book, setBook] = useState([])
+    const [inputValue, setInputValue] = useState('')
+    useEffect(() => {
+      fetch('http://localhost:3033/book/' + bookId)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setBook(data)
+        })
+    }, [])
+    const HandleSubmit = () => {
+      //fetch data from input
+      console.log(inputValue)
+
+    }
   return (
     <div>
         <HomeScreen />
@@ -20,15 +34,25 @@ function Borrow() {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">{bookId}</th>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
+                {book.map(book => (
+                    <tr key={book.ID}>
+                      <th scope="row">{book.ID}</th>
+                      <td>{book.Name}</td>
+                      <td>{book.Categories}</td>
+                      <td>{book.Amount}</td>
+                      {/* add 1 input */}
+                      <td>
+                        <input type="text" className="form-control" placeholder="Enter quantity" value ={inputValue} onChange={e => setInputValue(e.target.value)} />
+                      </td>
+                    </tr>
+                  ))}
                 
                 
               </tbody>
+                  {/* add 1 button submit with white color*/}
+                  <button type="button" className="btn btn-dark"
+                    onClick={()=>{HandleSubmit()}}
+                  >Submit</button>
             </table>
           </div>
         </div>
--- Frontend/my-app/src/screens/HomeScreen.js
+++ Frontend/my-app/src/screens/HomeScreen.js
@@ -1,26 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 function HomeScreen() {
   return (
-    //create navigation bar include home, login, myBags
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Library</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse show" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link active" href="/Home">Home <span className="sr-only"></span></a>
-                    <a className="nav-item nav-link" href="#">Login</a>
-                    <a className="nav-item nav-link" href="#">MyBags</a>
-                    <a className="nav-item nav-link ml-auto" href="#">Log out</a>
-                </div>
-            </div>
-        </nav>
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Library
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="/Home">
+              Home <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Login
+            </a>
+            <a className="nav-item nav-link" href="#">
+              MyBags
+            </a>
+            <a className="nav-item nav-link ml-auto" href="#">
+              Log out
+            </a>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
